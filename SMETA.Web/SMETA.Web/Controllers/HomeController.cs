ef76757a165d08b508eda8a4b1ff5886bc5f19f3
Dean <matthew.dean@CHAMPLAIN.EDU>
2017-02-07 12:05:15 -0500

    Contact form functionality part 1

--- SMETA.Web/SMETA.Web/Controllers/HomeController.cs
+++ SMETA.Web/SMETA.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,6 +19,27 @@ namespace SMETA.Web.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult PostContact(string contactName, string contactEmail, string contactMessage)
+        {
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 465);
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.Credentials = new System.Net.NetworkCredential("mjdean1994@gmail.com", "MmDea1N11");
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress(contactEmail, contactName);
+            mail.To.Add(new MailAddress("mjdean1994@gmail.com"));
+
+            mail.Subject = "New SMETA Contact Message";
+            mail.Body = contactMessage;
+
+            smtpClient.Send(mail);
+
+            return Json(new { success = true });
         }
     }
 }

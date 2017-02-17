b1118487cc467a0950981db400bf8e875fbb8b0d
Hanah Leo <hanah.leo@mymail.champlain.edu>
2017-02-16 19:08:33 -0500

    Graph is multiline for all attributes
    changed colors, need to:
    update dashboard
    add scatterplot
    breakup big functions

--- SMETA.Web/SMETA.Web/Controllers/HomeController.cs
+++ SMETA.Web/SMETA.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using SMETA.Web.Models;
 
 namespace SMETA.Web.Controllers
 {
@@ -40,6 +41,246 @@ namespace SMETA.Web.Controllers
             smtpClient.Send(mail);
 
             return Json(new { success = true });
+        }
+
+        public JsonResult GetData()
+        {
+            List<TweetData> data = new List<TweetData>();
+
+            data.Add(new TweetData
+            {
+                date = "23-Apr-12",
+                sentiment = 0.99f,
+                anger = 0.43f,
+                anticipation = 0.45f,
+                disgust = 0.73f,
+                fear = 0.28f,
+                joy = 0.49f,
+                sadness = 0.73f,
+                surprise = 0.41f,
+                trust = 0.58f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "20-Apr-12",
+                sentiment = 0.77f,
+                anger = 0.11f,
+                anticipation = .73f,
+                disgust = 0.44f,
+                fear = 0.86f,
+                joy = 0.76f,
+                sadness = 0.42f,
+                surprise = 0.35f,
+                trust = 0.68f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "19-Apr-12",
+                sentiment = 0.43f,
+                anger = 0.43f,
+                anticipation = 0.47f,
+                disgust = 0.35f,
+                fear = 0.11f,
+                joy = 0.28f,
+                sadness = 0.73f,
+                surprise = 0.41f,
+                trust = 0.99f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "18-Apr-12",
+                sentiment = 0.75f,
+                anger = 0.44f,
+                anticipation = 0.76f,
+                disgust = 0.12f,
+                fear = 0.37f,
+                joy = 0.63f,
+                sadness = 0.45f,
+                surprise = 0.24f,
+                trust = 0.18f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "17-Apr-12",
+                sentiment = 0.38f,
+                anger = 0.47f,
+                anticipation = 0.22f,
+                disgust = 0.37f,
+                fear = 0.19f,
+                joy = 0.88f,
+                sadness = 0.42f,
+                surprise = 0.11f,
+                trust = 0.01f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "16-Apr-12",
+                sentiment = 0.41f,
+                anger = 0.06f,
+                anticipation = 0.58f,
+                disgust = 0.47f,
+                fear = 0.11f,
+                joy = 0.09f,
+                sadness = 0.98f,
+                surprise = 0.45f,
+                trust = 0.36f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "13-Apr-12",
+                sentiment = 0.68f,
+                anger = 0.44f,
+                anticipation = 0.98f,
+                disgust = 0.04f,
+                fear = 0.93f,
+                joy = 0.86f,
+                sadness = 0.41f,
+                surprise = 0.88f,
+                trust = 0.08f
+            });
+
+            data.Add(new TweetData
+            {
+                date = "12-Apr-12",
+                sentiment = 0.36f,
+                anger = 0.46f,
+                anticipation = 0.36f,
+                disgust = 0.05f,
+                fear = 0.77f,
+                joy = 0.98f,
+                sadness = 0.36f,
+                surprise = 0.22f,
+                trust = 0.77f
+            });
+
+            data.Add(new TweetData
+            {
+                date = "11-Apr-12",
+                sentiment = 0.02f,
+                anger = 0.80f,
+                anticipation = 0.36f,
+                disgust = 0.59f,
+                fear = 0.18f,
+                joy = 0.63f,
+                sadness = 0.82f,
+                surprise = 0.76f,
+                trust = 0.66f
+            });
+
+            data.Add(new TweetData
+            {
+                date = "10-Apr-12",
+                sentiment = 0.32f,
+                anger = 0.56f,
+                anticipation = 0.28f,
+                disgust = 0.45f,
+                fear = 0.58f,
+                joy = 0.85f,
+                sadness = 0.22f,
+                surprise = 0.52f,
+                trust = 0.11f
+            });
+
+            data.Add(new TweetData
+            {
+                date = "9-Apr-12",
+                sentiment = 0.47f,
+                anger = 0.56f,
+                anticipation = .64f,
+                disgust = 0.27f,
+                fear = 0.77f,
+                joy = 0.85f,
+                sadness = 0.75f,
+                surprise = 0.24f,
+                trust = 0.43f
+            });
+
+            data.Add(new TweetData
+            {
+                date = "5-Apr-12",
+                sentiment = 0.91f,
+                anger = 0.56f,
+                anticipation = .05f,
+                disgust = 0.44f,
+                fear = 0.82f,
+                joy = 0.76f,
+                sadness = 0.43f,
+                surprise = 0.43f,
+                trust = 0.72f
+            });
+
+            data.Add(new TweetData
+            {
+                date = "4-Apr-12",
+                sentiment = 0.57f,
+                anger = 0.56f,
+                anticipation = 0.47f,
+                disgust = 0.73f,
+                fear = 0.18f,
+                joy = 0.84f,
+                sadness = 0.86f,
+                surprise = 0.74f,
+                trust = 0.52f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "3-Apr-12",
+                sentiment = 0.69f,
+                anger = 0.77f,
+                anticipation = .68f,
+                disgust = 0.32f,
+                fear = 0.78f,
+                joy = 0.14f,
+                sadness = 0.8f,
+                surprise = 0.58f,
+                trust = 0.47f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "2-Apr-12",
+                sentiment = 0.19f,
+                anger = 0.56f,
+                anticipation = 0.78f,
+                disgust = 0.49f,
+                fear = 0.58f,
+                joy = 0.01f,
+                sadness = 0.22f,
+                surprise = 0.52f,
+                trust = 0.11f
+            });
+
+
+            data.Add(new TweetData
+            {
+                date = "30-Mar-12",
+                sentiment = 0.5f,
+                anger = 0.99f,
+                anticipation = 0.98f,
+                disgust = 0.11f,
+                fear = 0.58f,
+                joy = 0.85f,
+                sadness = 0.10f,
+                surprise = 0.72f,
+                trust = 0.01f
+            });
+
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- SMETA.Web/SMETA.Web/Scripts/d3Functions.js
+++ SMETA.Web/SMETA.Web/Scripts/d3Functions.js
@@ -1,6 +1,10 @@
-﻿function getGraph() {
-    // dont hardcode data because fuck hardcoded data
-    /* use v3 instead of v4, more documentation on it */
+﻿/*
+ * CREATES AND POPULATES GRAPH 
+ * break up into functions later
+ */
+function getGraph() {
+    //CAPSTONE DOC DONT FORGET TO MENTION USE OF V3 INSTEAD OF V4 BC BETTER DOCUMENTATION
+    //NOTE!!! variables can be used as functions depending on what theyre set to, ex. see parseDate
 
     // Set the dimensions of the canvas / graph
     var margin = { top: 20, right: 0, bottom: 30, left: 50 },
@@ -28,12 +32,7 @@
         .orient("left")
         .ticks(5);
 
-    // Define the line
-    var sentiment_line = d3.svg.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.positive); });
-
-    // Add tooltip div, MAKE SIZE RESPONSIVE
+    // Add tooltip div, MAKE SIZE RESPONSIVE- NOT DONE
     var tooltip = d3.select("#graph")
         .append("div")
         .attr("class", "tooltip")
@@ -51,62 +50,183 @@
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-    //check mongodb query results, != JSON format
-    d3.json("../SampleData/data.json",
+
+    // Define different attribute lines
+    //use function conditionals for filter stuff later-just a note: HL
+    var sentiment_line = GetSentiment(x, y); 
+    var anger_line = GetAnger(x, y);
+    var anticipation_line = GetAnticipation(x, y);
+    var disgust_line = GetDisgust(x, y);
+    var fear_line = GetFear(x, y);
+    var joy_line = GetJoy(x, y);
+    var sadness_line = GetSadness(x, y);
+    var surprise_line = GetSurprise(x, y);
+    var trust_line = GetTrust(x, y);
+
+    //GetData will parse results and grab data with queries
+    d3.json("Home/GetData",
         function (error, data) {
-            console.log(data);
             data.forEach(function(d) {
                 d.date = parseDate(d.date);
-                d.positive = +d.positive;
+                d.sentiment = +d.sentiment;
+                d.anger = +d.anger;
+                d.anticipation = +d.anticipation;
+                d.disgust = +d.disgust;
+                d.fear = +d.fear;
+                d.joy = +d.joy;
+                d.sadness = +d.sadness;
+                d.trust = +d.trust;
             });
 
             // Scale the range of the data
-            x.domain(d3.extent(data, function(d) { return d.date; }));
-            y.domain([0, d3.max(data, function(d) { return d.positive; })]);
+            x.domain(d3.extent(data, function (d) { return d.date; }));
 
-            // Add data line
-            svg.append("path")
-                .attr("class", "line")
-                .attr("d", sentiment_line(data));
+
+            //function: COMPARE MAX ATTR VALUES
+            //set default to max sentiment value of sample set -> 0.99
+            y.domain([0, d3.max(data, function(d) { return d.sentiment; })]);
+
+            // Add data line for each attribute
+            drawLine(svg, sentiment_line, data, "sentiment");
+            drawLine(svg, anger_line, data, "anger");
+            drawLine(svg, anticipation_line, data, "anticipation");
+            drawLine(svg, disgust_line, data, "disgust");
+            drawLine(svg, fear_line, data, "fear");
+            drawLine(svg, joy_line, data, "joy");
+            drawLine(svg, sadness_line, data, "sadness");
+            drawLine(svg, surprise_line, data, "surprise");
+            drawLine(svg, trust_line, data, "trust");
 
             /* 
-            * Scatterplot 
+            * Scatterplot JUST FOR SENTIMENT FOR NOW
             * So tooltip can use data values
             */
             svg.selectAll("dot")
                 .data(data)
                 .enter()
                 .append("circle")
-                    .attr("r", 3) //make it bigger when hovering?
-                    .attr("cx", function(d) { return x(d.date); })
-                    .attr("cy", function(d) { return y(d.positive); })
-                    .attr("id", "dataPlots")
-                    // tooltip bit
-                    .on("mouseover", function (d) {
-                        tooltip.transition()
-                            .duration(200)
-                            .style("opacity", .9);
-                       
-                        tooltip.html(formatDate(d.date) + "<br/>Positivity: " + d.positive)
-                            .style("left", (d3.event.pageX - 70) + "px")
-                            .style("top", (d3.event.pageY - 220) + "px");
-                        //make radius of circle bigger.
-                    })
-                    .on("mouseout", function (d) {
-                        tooltip.transition()
-                            .duration(250)
-                            .style("opacity", 0);
-                    });
+                .attr("r", 3) //make it bigger when hovering?
+                .attr("cx", function(d) { return x(d.date); })
+                .attr("cy", function(d) { return y(d.sentiment); })
+                .attr("id", "dataPlots")
+                .on("mouseover", function (d) {
 
-            // Add the X Axis
+                    /*changes all dots, needs to be individual
+                    dot.transition()
+                        .duration(1000)
+                        .attr("r", 6);*/
+
+                    tooltip.transition()
+                        .duration(250)
+                        .style("opacity", .9);
+                       
+                    tooltip.html(formatDate(d.date) + "<br/>Sentiment: " + d.sentiment)
+                        .style("left", (d3.event.pageX - 70) + "px")
+                        .style("top", (d3.event.pageY - 220) + "px");
+
+                })
+                .on("mouseout", function (d) {
+                    tooltip.transition()
+                        .duration(250)
+                        .style("opacity", 0);
+                });
+
+            //* Add X Axis
             svg.append("g")
                 .attr("transform", "translate(0," + height + ")")
                 .call(xAxis);
 
-            // Add the Y Axis
+            //* Add Y Axis
             svg.append("g")
                 .call(yAxis);
+
+            // Legends are above on dashboard, change colors
 
         });
 }
 
+
+
+
+
+
+
+/*
+* GETATTRIBUTE FUNCTION RETURN FOR SIMPLICITY
+* Way to simplify it somehow but after functionality
+*/
+
+function GetSentiment(x, y) {
+    return d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function (d) { return y(d.sentiment); })
+        .interpolate("monotone");
+}
+
+function GetAnger(x, y) {
+    return d3.svg.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.anger); })
+    .interpolate("monotone");
+}
+
+function GetAnticipation(x, y) {
+    return d3.svg.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.anticipation); })
+    .interpolate("monotone");
+}
+
+function GetDisgust(x, y) {
+    return d3.svg.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.disgust); })
+    .interpolate("monotone");
+}
+
+function GetFear(x, y) {
+    return d3.svg.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.fear); })
+    .interpolate("monotone");
+}
+
+function GetJoy(x, y) {
+    return d3.svg.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.joy); })
+    .interpolate("monotone");
+}
+
+function GetSadness(x, y) {
+    return d3.svg.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.sadness); })
+    .interpolate("monotone");
+}
+
+function GetSurprise(x, y) {
+    return d3.svg.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.surprise); })
+    .interpolate("monotone");
+}
+
+function GetTrust(x, y) {
+    return d3.svg.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.trust); })
+    .interpolate("monotone");
+}
+
+
+/*
+ * Function to actually draw graphs
+ * need svg
+ *
+ */
+function drawLine(svg, drawAtt, data, attribute) {
+    svg.append("path")
+        .attr("class", attribute)
+        .attr("d", drawAtt(data));
+}

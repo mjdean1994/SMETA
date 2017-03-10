d4160bcda2135f4af0592d32ffb452a807fe910d
Hanah Leo <hanah.leo@mymail.champlain.edu>
2017-03-09 20:27:35 -0500

    took out attributes, added sentiment split toggler

--- SMETA.Web/SMETA.Web/Scripts/d3Functions.js
+++ SMETA.Web/SMETA.Web/Scripts/d3Functions.js
@@ -1,10 +1,8 @@
 ﻿/*
  * CREATES AND POPULATES GRAPH 
- * break up into functions later
  */
 function getGraph() {
     //CAPSTONE DOC DONT FORGET TO MENTION USE OF V3 INSTEAD OF V4 BC BETTER DOCUMENTATION
-    //NOTE!!! variables can be used as functions depending on what theyre set to, ex. see parseDate
 
     // Set the dimensions of the canvas / graph
     var margin = { top: 20, right: 0, bottom: 30, left: 50 },
@@ -41,32 +39,12 @@ function getGraph() {
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-
-    // Define different attribute lines
-    // Use variables for comprehension
-    var sentiment_line = SentimentLine(x, y);
-    var anger_line = AngerLine(x, y);
-    var anticipation_line = AnticipationLine(x, y);
-    var disgust_line = DisgustLine(x, y);
-    var fear_line = FearLine(x, y);
-    var joy_line = JoyLine(x, y);
-    var sadness_line = SadnessLine(x, y);
-    var surprise_line = SurpriseLine(x, y);
-    var trust_line = TrustLine(x, y);
-
     //GetData will parse results and grab data with queries
     d3.json("Home/GetData",
         function (error, data) {
             data.forEach(function(d) {
                 d.date = parseDate(d.date);
                 d.sentiment = +d.sentiment;
-                d.anger = +d.anger;
-                d.anticipation = +d.anticipation;
-                d.disgust = +d.disgust;
-                d.fear = +d.fear;
-                d.joy = +d.joy;
-                d.sadness = +d.sadness;
-                d.trust = +d.trust;
             });
 
             // Scale the range of the data
@@ -78,29 +56,12 @@ function getGraph() {
             y.domain([0, d3.max(data, function(d) { return d.sentiment; })]);
 
             // Add data line for each attribute
-            drawLine(svg, sentiment_line, data, "sentiment");
-            drawLine(svg, anger_line, data, "anger");
-            drawLine(svg, anticipation_line, data, "anticipation");
-            drawLine(svg, disgust_line, data, "disgust");
-            drawLine(svg, fear_line, data, "fear");
-            drawLine(svg, joy_line, data, "joy");
-            drawLine(svg, sadness_line, data, "sadness");
-            drawLine(svg, surprise_line, data, "surprise");
-            drawLine(svg, trust_line, data, "trust");
+            drawLine(svg, SentimentLine(x, y), data, "sentiment");
 
             /* 
-            * Scatterplot JUST FOR SENTIMENT FOR NOW
             * So tooltip can use data values
             */
-            addScatterPlot(svg, data, "sentiment", x, y);
-            addScatterPlot(svg, data, "anger", x, y);
-            addScatterPlot(svg, data, "anticipation", x, y);
-            addScatterPlot(svg, data, "disgust", x, y);
-            addScatterPlot(svg, data, "fear", x, y);
-            addScatterPlot(svg, data, "joy", x, y);
-            addScatterPlot(svg, data, "sadness", x, y);
-            addScatterPlot(svg, data, "surprise", x, y);
-            addScatterPlot(svg, data, "trust", x, y);
+            addScatterPlot(svg, data, "Sentiment", x, y);
          
 
             //* Add X Axis
@@ -119,9 +80,11 @@ function getGraph() {
 
 
 
-
-
-
+/***********************************************************
+ *
+ *            ------ SUPPORT FUNCTIONS -----
+ *
+ **********************************************************/
 
 /*
 * GETATTRIBUTE FUNCTION RETURN FOR SIMPLICITY
@@ -135,67 +98,10 @@ function SentimentLine(x, y) {
         .interpolate("monotone");
 }
 
-function AngerLine(x, y) {
-    return d3.svg.line()
-    .x(function (d) { return x(d.date); })
-    .y(function (d) { return y(d.anger); })
-    .interpolate("monotone");
-}
-
-function AnticipationLine(x, y) {
-    return d3.svg.line()
-    .x(function (d) { return x(d.date); })
-    .y(function (d) { return y(d.anticipation); })
-    .interpolate("monotone");
-}
-
-function DisgustLine(x, y) {
-    return d3.svg.line()
-    .x(function (d) { return x(d.date); })
-    .y(function (d) { return y(d.disgust); })
-    .interpolate("monotone");
-}
-
-function FearLine(x, y) {
-    return d3.svg.line()
-    .x(function (d) { return x(d.date); })
-    .y(function (d) { return y(d.fear); })
-    .interpolate("monotone");
-}
-
-function JoyLine(x, y) {
-    return d3.svg.line()
-    .x(function (d) { return x(d.date); })
-    .y(function (d) { return y(d.joy); })
-    .interpolate("monotone");
-}
-
-function SadnessLine(x, y) {
-    return d3.svg.line()
-    .x(function (d) { return x(d.date); })
-    .y(function (d) { return y(d.sadness); })
-    .interpolate("monotone");
-}
-
-function SurpriseLine(x, y) {
-    return d3.svg.line()
-    .x(function (d) { return x(d.date); })
-    .y(function (d) { return y(d.surprise); })
-    .interpolate("monotone");
-}
-
-function TrustLine(x, y) {
-    return d3.svg.line()
-    .x(function (d) { return x(d.date); })
-    .y(function (d) { return y(d.trust); })
-    .interpolate("monotone");
-}
-
 
 /*
  * Function to actually draw graphs
  * need svg
- *
  */
 function drawLine(svg, drawAtt, data, attribute) {
     svg.append("path")
@@ -203,18 +109,13 @@ function drawLine(svg, drawAtt, data, attribute) {
         .attr("d", drawAtt(data));
 }
 
-
-/*****************************************************************
- * 
- * Scatterplot element - Function that actually gets called
- * 
+/*
  * PARAMETERS:
  *   svg - the svg to append to
  *   data - the data
  *   attribute - for displaying in tooltips
  *   getAtt - function to get attribute value
- *
- ****************************************************************/
+ */
 function addScatterPlot(svg, data, attribute, x, y) {
 
     // Add tooltip div, MAKE SIZE RESPONSIVE- NOT DONE
@@ -224,7 +125,7 @@ function addScatterPlot(svg, data, attribute, x, y) {
         .style("opacity", 0);
 
     //Format date/time - SEE TOOLTIPS
-    var formatDate = d3.time.format("Date: %x \nTime: %H:%M%p");
+    var formatDate = d3.time.format("Date: %x <br/> Time: %H:%M%p");
 
     return svg.selectAll("dot")
         .data(data)
@@ -251,16 +152,20 @@ function addScatterPlot(svg, data, attribute, x, y) {
         })
         .on("mouseout", function (d) {
             tooltip.transition()
-                .duration(250)
+                .duration(300)
                 .style("opacity", 0);
         });
 }
 
-//if attribute == string, then use that function -> switch statements?
 
-/***********************************************
- * 
- *            SUPPORT FUNCTIONS
- *
- ***********************************************/
+function switchGraphData() {
+    var elem = document.getElementById("graphView");
 
+    if (elem.textContent == "Compound") {
+        elem.textContent = "Split";
+        //other things
+    } else {
+        elem.textContent = "Compound";
+        //OTHER FUNCTION FOR RELOADING GRAPH
+    }
+}
